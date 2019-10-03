68ccff2134580f514266cd101ea436596c280523
ashwinith <ashwinith@gmail.com>
2019-10-03 11:43:34 +0530

    code commit

---
+++ Variable.js
@@ -0,0 +1,17 @@
+let name , copyName; // We can declare multiple variables in one go
+
+name = "Javascript";
+
+copyName=name;
+
+alert(copyName);
+
+
+
+
+
+var userName = prompt("Please enter your name", '');
+
+
+    document.write("<h1>Hello "+userName+"</h1>");
+
